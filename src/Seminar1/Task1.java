3d46fb448c3ab0dc57a8b4c026c9013777714e24
Yuri Konev <crazy_clown93@mail.ru>
2024-07-23 22:25:37 +0300

    first commit

---
+++ src/Seminar1/Task1.java
@@ -0,0 +1,17 @@
+package Seminar1;
+
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Locale;
+
+//public class Task1 {
+//    public static void main(String[] args) {
+//        LocalDateTime.now() = LocalDateTime.now();
+//        SimpleDateFormat sdf = new SimpleDateFormat()
+//
+//        System.out.println();
+//
+//        System.out.println("Сегодня и сейчас: " + now());
+//    }
+//}
---
+++ src/Seminar1/Task3.java
@@ -0,0 +1,17 @@
+package Seminar1;
+
+import java.io.File;
+import java.io.FileWriter;
+
+public class Task3 {
+    public static void main(String[] args) {
+
+    }
+
+    private void writeToFile(String path, String content) {
+
+//        File file = new File(path);
+        FileWriter fw = FileWе
+    }
+
+}
