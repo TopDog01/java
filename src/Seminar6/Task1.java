090183b86f631bb47814d5f526813c664a2d4939
Yuri Konev <crazy_clown93@mail.ru>
2024-08-25 20:27:13 +0300

    Sem6Task1

--- src/Seminar6/Task1.java
+++ src/Seminar6/Task1.java
@@ -1,27 +1,39 @@
 package Seminar6;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
 
 public class Task1 {
+    private static int[] arr = new int[10];
 
+    public static void main(String[] args) {
 
-    public class Main {
-        public static void main(String[] args) {
+        Task1 task1 = new Task1();
+        task1.fillArr();
 
-            HashSet<Integer> set = new HashSet<>();
+        System.out.println(Arrays.toString(arr));
+        System.out.println(task1.getUniquePercent(arr));
 
-
-            set.add(1);
-            set.add(2);
-            set.add(3);
-            set.add(2);
-            set.add(4);
-            set.add(5);
-            set.add(6);
-            set.add(3);
-
-            System.out.println("Содержимое множества: " + set);
-        }
     }
 
+    private String getUniquePercent(int[] arr) {
+
+        Set<Integer> set = new HashSet<>();
+
+        for (int i : arr) {
+            set.add(i);
+        }
+
+        return set.size() * 100 / (double) arr.length + " %";
+
+    }
+
+
+    private void fillArr() {
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 24);
+        }
+    }
 }
